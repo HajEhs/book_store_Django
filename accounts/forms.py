4e75ba0dafb76cd6b3ce23b37d47f7370ad84c67
Ehsan <ehsan.doroudian@gmail.com>
2024-02-11 17:59:20 +0330

    create CustomUserChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,3 +6,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUserModel
         fields = ("username", "email", "age", )
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUserModel
+        fields = ("username", "email", "age",)
+
