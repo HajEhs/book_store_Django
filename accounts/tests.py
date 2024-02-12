f9ab6015c1eacdefb8fb7f4e931b5b6d77f60d83
Ehsan <ehsan.doroudian@gmail.com>
2024-02-12 14:10:34 +0330

    test urls, names, templates

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SignUpPageTset(TestCase):
+    def test_signup_page_by_url(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_page_by_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_page_template_used(self):
+        response = self.client.get(reverse("signup"))
+        self.assertTemplateUsed(response, "registration/signup_page.html")
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestPages(TestCase):
+    def test_home_page_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_by_url(self):
+        response = self.client.get("/home/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Home Page")
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "pages/home_page.html")
