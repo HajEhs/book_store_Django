e0bea4134a436da5175361f5fab43e888efa5837
Ehsan <ehsan.doroudian@gmail.com>
2024-02-14 21:36:57 +0330

    crispy forms

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party app
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    # local apps
     "accounts",
     "pages",
 ]
@@ -123,6 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# authentication
 AUTH_USER_MODEL = "accounts.CustomUserModel"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# crispy
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
